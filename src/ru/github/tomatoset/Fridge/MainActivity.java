31371d54ba5da64342c9723b5473b123e7b716ac
tomatoset <mtripilec@gmail.com>
2015-09-05 13:34:50 +0600

    Two

---
+++ src/ru/github/tomatoset/Fridge/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.github.tomatoset.Fridge;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by barbarossa on 05.09.15.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
